eb86313f72b6e11e557ead0979472164fec0c054
Bayo Erinle <berinle@gmail.com>
2013-03-22 09:42:26 -0400

    initial checkin

---
+++ src/test/groovy/com/spockmaven/TestSpec.groovy
@@ -0,0 +1,14 @@
+package com.spockmaven
+
+import spock.lang.Specification
+
+/**
+ * @author berinle
+ */
+class TestSpec extends Specification {
+
+    def "test something cool"(){
+        expect:
+        1==2
+    }
+}
---
+++ src/test/java/com/spockmaven/Test1.java
@@ -0,0 +1,14 @@
+package com.spockmaven;
+
+import static org.junit.Assert.fail;
+
+/**
+ * @author berinle
+ */
+public class Test1 {
+
+    @org.junit.Test
+    public void testName() throws Exception {
+        fail("todo");
+    }
+}
